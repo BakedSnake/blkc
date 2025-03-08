9a44508d1cda73b0bfead2a5bd04d5dec0842f14
bakedsnake <gilferrandm@gmail.com>
2025-03-09 00:50:40 +0100

    remove unnecessary config_ssh function

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,21 @@
 use std::fs::File;
-use std::path::Path;
 use std::process::Command;
-use std::io::{Read, BufRead};
+use std::io::Read;
 use serde::{Deserialize, Serialize};
 
 pub const ROOT_COLOR_PREFIX: &str = "\x1b[33m";
 pub const MAIN_COLOR_PREFIX: &str = "\x1b[32m";
 pub const MAIN_COLOR_SUFFIX: &str = "\x1b[0m";
+
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+pub struct Server {
+    pub id: i32,
+    pub label: &'static str,
+    pub name: &'static str,
+    pub user: &'static str,
+    pub address: &'static str,
+    pub sshport: &'static str
+}
 
 pub fn print_server_details(vec_data: Vec<Server>, server_name: &'static str) {
     for server in vec_data {
@@ -61,38 +70,6 @@ pub fn get_passkey(server_name: String) -> std::io::Result<String> {
     }
 }
 
-pub fn config_ssh() -> std::io::Result<String>{
-    let home_cfg = std::env::var("HOME").unwrap().to_string() + "/.config/blkc/blkc.conf";
-    let path_user = Path::new(&home_cfg);
-    let path;
-    if path_user.is_file() {
-        path = path_user;
-    } else {
-        println!("No config file fond at ~/.config/blkc.conf");
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::NotFound,
-            "Error...",
-        ))
-    }
-    let file = File::open(path)?;
-    let reader = std::io::BufReader::new(file);
-    for line in reader.lines() {
-        let line = line?;
-        let parts: Vec<&str> = line.splitn(2, '=').collect();
-        if parts.len() == 2 {
-            let key = parts[0].trim();
-            let value = parts[1].trim();
-            if key == "ssh_key" {
-                return Ok(value.to_string());
-            }
-        }
-    }
-    Err(std::io::Error::new(
-        std::io::ErrorKind::NotFound,
-        "Error...",
-    ))
-}
-
 pub fn server_list<'a>() -> std::io::Result<&'a str> {
     let list_path = std::env::var("HOME").unwrap().to_string() + "/.config/blkc/list.json";
     let mut file = File::open(String::from(list_path)).expect("Failed to open file.");
@@ -102,12 +79,3 @@ pub fn server_list<'a>() -> std::io::Result<&'a str> {
     Ok(static_str.try_into().expect("try failed"))
 }
 
-#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
-pub struct Server { 
-    pub id: i32,
-    pub label: &'static str,
-    pub name: &'static str,
-    pub user: &'static str,
-    pub address: &'static str,
-    pub sshport: &'static str 
-}
