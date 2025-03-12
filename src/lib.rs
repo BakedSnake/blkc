80a63e58c27dce4a5418df2a384247c64be54d5b
bakedsnake <gilferrandm@gmail.com>
2025-03-12 19:44:10 +0100

    update print server details arguments

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ impl Server {
     }
 }
 
-pub fn print_server_details(servers: Vec<Server>, server_name: &'static str) {
+pub fn print_server_details(server_name: &'static str, _command: &'static str, _: &'static Vec<String>, servers: &'static Vec<Server>) {
     for server in servers {
         match !server_name.is_empty() {
             true => match server.name == server_name {
