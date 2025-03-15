4b9bf11a61be845d9b2e3594305e7912fd9b344a
bakedsnake <gilferrandm@gmail.com>
2025-03-15 13:42:57 +0100

    make commands and options not depend on argument length

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ struct Command {
     name:           &'static str,
     description:    &'static str,
     option:         &'static str,
-    run:            fn(&'static str, &'static str, &'static Vec<String>, &'static Vec<Server>)
+    run:            fn(&'static str, &'static str, &'static str, &'static Vec<String>, &'static Vec<Server>)
 }
 
 static DESCRIPTIONS: [&str; 7] = [
@@ -21,6 +21,13 @@ static DESCRIPTIONS: [&str; 7] = [
     "Run a command as root on a multiple remote host.",
     "Print help menu.",
     "Show version."
+];
+
+static VALID_OPTIONS: [&str; 4] = [
+    "--name",
+    "--label",
+    "-n",
+    "-l"
 ];
 
 static COMMANDS: [Command; 14] = [
@@ -53,18 +60,38 @@ fn main() {
     let servers: Vec<Server> = serde_json::from_str(servers_json).expect("Failed to deserialize.");
     let static_servers: &'static Vec<Server> = Box::leak(Box::new(servers));
 
-    let command = if static_args.len() > 1 { static_args[1].trim() } else { "" };
-    let opt     = if static_args.len() > 2 { static_args[2].trim() } else { "" };
-    let query   = if static_args.len() > 3 { static_args[3].trim() } else { "" };
-    let rm_cmd  = if static_args.len() > 4 { static_args[4].trim() } else { "" };
+    let mut cmd_name_list: Vec<&str> = vec![];
+    for command in COMMANDS.iter() {
+        cmd_name_list.push(command.name);
+    }
+
+    let command = match static_args.iter().find(|arg| cmd_name_list.contains(&arg.trim())) {
+        Some(arg) => arg,
+        None => { eprintln!("Error: Command not found."); exit(1) }
+    };
+
+    let opt = match static_args.iter().find(|arg| VALID_OPTIONS.contains(&arg.trim())) {
+        Some(arg) => arg,
+        None => ""
+    };
+
+    let query = match static_args.windows(2).find(|pair| &pair[0].trim() == &opt) {
+        Some(pair) => &pair[1],
+        None => ""
+    };
+
+    let rm_cmd = match static_args.windows(2).find(|pair| &pair[0].trim() == &query) {
+        Some(pair) => &pair[1],
+        None => ""
+    };
 
     match static_args.len() >= 5 {
         true => match COMMANDS.iter().find(|cmd| cmd.name == command && cmd.option == opt) {
-            Some(cmd) => (cmd.run)(&query, rm_cmd, static_colors, static_servers),
+            Some(cmd) => (cmd.run)(&query, rm_cmd, &opt, static_colors, static_servers),
             None => { eprintln!("Error: Command not found."); exit(1) }
         },
         false => match COMMANDS.iter().find(|cmd| cmd.name == command) {
-            Some(cmd) => (cmd.run)(&opt, "", static_colors, &static_servers),
+            Some(cmd) => (cmd.run)(&query, "", &opt, static_colors, &static_servers),
             None => { eprintln!("Error: Command not found."); exit(1) }
         },
     }
